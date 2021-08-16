1f04639bc4f005baeafc1abf58ebeff844883058
Manuel Spigolon <behemoth89@gmail.com>
2021-08-16 19:04:04 +0200

    add ajv-i18n tests

--- test/plugins.test.js
+++ test/plugins.test.js
@@ -6,6 +6,7 @@ const AjvCompiler = require('../index')
 
 const ajvFormats = require('ajv-formats')
 const ajvErrors = require('ajv-errors')
+const localize = require('ajv-i18n')
 
 t.test('Format Baseline test', async (t) => {
   const app = buildApplication({
@@ -129,6 +130,52 @@ t.test('Custom error messages', (t) => {
     t.error(err)
     t.equal(res.statusCode, 400)
     t.match(res.json().message, errorMessage.additionalProperties)
+  })
+})
+
+t.test('Custom i18n error messages', (t) => {
+  t.plan(3)
+
+  const app = buildApplication({
+    customOptions: {
+      allErrors: true,
+      messages: false
+    },
+    plugins: [ajvFormats]
+  })
+
+  app.post('/', {
+    handler: () => { t.fail('dont call me') },
+    schema: {
+      body: {
+        type: 'object',
+        required: ['foo'],
+        properties: {
+          foo: { type: 'integer' }
+        }
+      }
+    }
+  })
+
+  app.setErrorHandler((error, request, reply) => {
+    t.pass('Error handler executed')
+    if (error.validation) {
+      localize.ru(error.validation)
+      reply.status(400).send(error.validation)
+      return
+    }
+    t.fail('not other errors')
+  })
+
+  app.inject({
+    method: 'POST',
+    url: '/',
+    payload: {
+      foo: 'string'
+    }
+  }, (err, res) => {
+    t.error(err)
+    t.equal(res.json()[0].message, 'должно быть integer')
   })
 })
 
