78b2db70c0c57c0e24f4addcc169ac254d4dee97
uzlopak <aras.abbasi@googlemail.com>
2022-12-05 16:45:20 +0100

    initial

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,22 @@
-import { default as _ajv } from "ajv";
+import { AnySchema,  default as _ajv, Options as AjvOptions, ValidateFunction } from "ajv";
+import { default as AjvJTD, JTDOptions } from "ajv/dist/jtd";
 import type { Options, ErrorObject } from "ajv";
 
-type AjvCompilerFn = typeof AjvCompiler
+type AjvSerializerGenerator = typeof AjvCompiler
 
 declare namespace AjvCompiler {
   export type { Options, ErrorObject }
   export type Ajv = _ajv;
-  
+
+  type AjvJTDCompile = AjvJTD['compileSerializer']
+  type AjvCompile = Ajv['compile']
+
+  export type BuildSerializerFromPool = (externalSchemas: any, serializerOpts: JTDOptions) => AjvJTDCompile
+
+  export type BuildCompilerFromPool =
+    ((externalSchemas: { [key: string]: AnySchema | AnySchema[] }, options?: { mode: 'JTD', customOptions?: JTDOptions }) => ReturnType<AjvCompile>) |
+    ((externalSchemas: { [key: string]: AnySchema | AnySchema[] }, options?: { mode?: undefined, customOptions?: AjvOptions }) => ReturnType<AjvCompile>)
+
   export { StandaloneValidator }
 
   export const AjvReference: Symbol
@@ -25,22 +35,31 @@ declare namespace AjvCompiler {
     schema?: unknown,
   }
 
-  export interface StandaloneOptions {
-    readMode: Boolean,
-    storeFunction?(opts: RouteDefinition, schemaValidationCode: string): void,
-    restoreFunction?(opts: RouteDefinition): void,
+  export type StandaloneOptionsReadModeOn = {
+    readMode: true;
+    restoreFunction?: StandaloneRestoreFunction
   }
 
-  export type ValidatorCompiler = (
-    externalSchemas: unknown,
-    options: Options
-  ) => Ajv;
+  export type StandaloneOptionsReadModeOff = {
+    readMode?: false | undefined;
+    storeFunction?: StandaloneStoreFunction;
+  }
 
-  export const AjvCompiler: AjvCompilerFn
+  export type StandaloneOptions = StandaloneOptionsReadModeOn | StandaloneOptionsReadModeOff
+
+  export type ValidatorCompiler = BuildCompilerFromPool | BuildSerializerFromPool
+
+  export type StandaloneRestoreFunction = (opts: RouteDefinition) => ValidateFunction
+
+  export type StandaloneStoreFunction = (opts: RouteDefinition, schemaValidationCode: string) => void
+
+  export const AjvCompiler: AjvSerializerGenerator
   export { AjvCompiler as default }
 }
 
-declare function AjvCompiler(): AjvCompiler.ValidatorCompiler;
+declare function AjvCompiler<T = unknown>(opts?: { jtdSerializer: true }): AjvCompiler.BuildSerializerFromPool
+declare function AjvCompiler<T = unknown>(opts?: { jtdSerializer?: false | undefined }): AjvCompiler.BuildCompilerFromPool
+
 declare function StandaloneValidator(options: AjvCompiler.StandaloneOptions): AjvCompiler.ValidatorCompiler;
 
 export = AjvCompiler
--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,14 +1,15 @@
-import { AnySchemaObject } from "ajv";
+import { AnySchemaObject, ValidateFunction } from "ajv";
 import { expectAssignable, expectType } from "tsd";
-import  ValidatorSelector, { AjvReference, ValidatorCompiler, StandaloneValidator, RouteDefinition, ErrorObject } from "..";
+import AjvCompiler, { AjvReference, ValidatorCompiler, StandaloneValidator, RouteDefinition, ErrorObject, BuildCompilerFromPool, BuildSerializerFromPool } from "..";
 
-const compiler = ValidatorSelector();
-expectType<ValidatorCompiler>(compiler);
+const compiler = AjvCompiler({});
+expectType<BuildCompilerFromPool>(compiler);
 
 const reader = StandaloneValidator({
   readMode: true,
   restoreFunction: (route: RouteDefinition) => {
     expectAssignable<RouteDefinition>(route)
+    return {} as ValidateFunction
   },
 });
 expectType<ValidatorCompiler>(reader);
@@ -33,3 +34,42 @@ expectType<unknown>(({} as ErrorObject).schema)
 expectType<string>(({} as ErrorObject).schemaPath)
 
 expectType<Symbol>(AjvReference)
+
+{
+  const jtdSchema = {
+    discriminator: 'version',
+    mapping: {
+      1: {
+        properties: {
+          foo: { type: 'uint8' }
+        }
+      },
+      2: {
+        properties: {
+          foo: { type: 'string' }
+        }
+      }
+    }
+  }
+
+  const externalSchemas1 = {
+    foo: {
+      definitions: {
+        coordinates: {
+          properties: {
+            lat: { type: 'float32' },
+            lng: { type: 'float32' }
+          }
+        }
+      }
+    }
+  }
+
+  const factory = AjvCompiler({ jtdSerializer: true })
+  expectType<BuildSerializerFromPool>(factory)
+  const compiler = factory(externalSchemas1, {})
+  expectAssignable<Function>(compiler)
+  const serializeFunc = compiler({ schema: jtdSchema })
+  expectType<(data: unknown) => string>(serializeFunc)
+  expectType<string>(serializeFunc({ version: '1', foo: 42 }))
+}
